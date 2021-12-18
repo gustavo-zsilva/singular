39dc56de547f9e9adcdfc05e77753c4af95949fa
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-12-18 17:13:48 -0300

    Added About section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,16 +1,66 @@
-import { Container, Section, Text, LinearGradient } from "./styles"
+import Image from 'next/image'
+
+import { FiShare2, FiCode } from 'react-icons/fi'
+import { Container } from "./styles"
 
 export function About() {
     return (
         <Container>
-            <Section>
-                <LinearGradient />
-                <Text>Texto</Text>
-            </Section>
-            <Section>
-                <LinearGradient />
-                <Text>Texto</Text>
-            </Section>
+            
+            <main>
+                <strong>Motivação</strong>
+            
+                <span>
+                    Por causa da pandemia, muitas pessoas precisam e precisarão de reforço escolar.
+                    É isso que o Singular procura resolver, por meio de uma plataforma onde se pode encontrar conteúdos escolares do ensino médio,
+                    dando oportunidade de professores e alunos compartilharem seus conhecimentos sobre cada matéria.
+                </span>
+
+                <Image
+                    width={300}
+                    height={300}
+                    src="/studying.svg"
+                    alt="Studying"
+                />
+            </main>
+
+            <section>
+                <span>Comunidade</span>
+                <picture>
+                    <FiShare2 size={42} />
+                </picture>
+                <p>
+                    Singular conta com a comunidade para produzir conteúdos, compartilhar seu conhecimento e ajudar uns aos outros.
+                </p>
+                <p>
+                    Tem algo que você sabe, que acha que seria útil para outros? Não precisa ser especialista em tudo para fazer a diferença.
+                </p>
+            </section>
+            <section>
+                <span>Open-Source</span>
+                <picture>
+                    <FiCode size={42} />
+                </picture>
+
+                <p>
+                    A plataforma será totalmente código aberto, e tudo o que faz o aplicativo funcionar por baixo dos panos estará disponível 
+                    no Github.
+                </p>
+                <p>
+                    Isso torna uma plataforma muito mais estável e com menos chance de haver vazamentos ou <i>hacks</i>, além de 
+                    atualizações frequentes.
+                </p>
+            </section>
+            <section>
+                <picture>
+                    <FiShare2 size={42} />
+                </picture>
+
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maiores quas ea, sunt sit nemo expedita atque aliquam cumque! Est dolorem itaque saepe! Beatae quas voluptatem nesciunt autem voluptates accusamus!
+                </p>
+            </section>
+            
         </Container>
     )
 }
--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -1,24 +1,108 @@
-import { styled } from "@stitches/react";
+import { styled } from "../../styles/theme";
 
 export const Container = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: 'auto 1fr',
+    gap: '4rem 6rem',
+
     height: '100vh',
-    justifyContent: 'center',
-})
+    width: '100%',
+    padding: '2rem 0',
 
-export const Text = styled('strong', {
-    fontSize: '1.2rem'
-})
+    backgroundImage: 'url(/wave.svg)',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'bottom',
 
-export const LinearGradient = styled('div', {
-    width: '4px',
-    height: '100%',
-    background: 'linear-gradient(#FF3366, #20A4F3)'
-})
+    'main': {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 'fit-content',
 
-export const Section = styled('section', {
-    display: 'flex',
-    gap: '1rem',
-    padding: '.8rem'
+        gap: '2rem',
+        gridColumn: 'span 3',
+        
+        background: '$primaryColor',
+        padding: '1rem 2rem',
+        borderRadius: '.2rem',
+        position: 'relative',
+
+        'strong': {
+            position: 'absolute',
+            top: '0',
+            paddingTop: '1rem',
+            
+            fontSize: '1.5rem',
+        },
+
+        'span': {
+            display: 'flex',
+            flexDirection: 'column',
+
+            lineHeight: '1.6rem',
+            fontSize: '1.2rem',
+
+            maxWidth: '60rem',
+            paddingLeft: '1.2rem',
+            borderLeft: '4px solid $baseColor',
+        }
+    },
+
+    'section': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        gap: '2rem',
+
+        border: '3px solid $primaryColor',
+        padding: '0 1rem 2rem 1rem',
+        borderRadius: '.2rem',
+        position: 'relative',
+
+        cursor: 'pointer',
+
+        'span': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+
+            padding: '1rem .8rem',
+            border: '3px solid $primaryColor',
+            borderTop: 'none',
+            borderRadius: '0 0 .8rem .8rem',
+
+            background: '$baseColor',
+            transform: 'translateY(-3px)',
+            marginLeft: '.8rem',
+
+            fontWeight: 'bold',
+        },
+
+        'picture': {
+            background: '$baseColor',
+            transform: 'translateY(-3px)',
+            border: '3px solid $primaryColor',
+            borderTop: 'none',
+            
+            padding: '3rem 1.5rem',
+            borderRadius: '0 0 999px 999px',
+            lineHeight: '0',
+
+            '&:hover': {
+
+            }
+        },
+
+        'p': {
+            lineHeight: '1.5rem',
+            fontSize: '1.2rem'
+        },
+
+        '&:focus-within': {
+            border: '3px solid $secondaryColor',
+        }
+    }
 })
