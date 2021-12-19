81bc2841b9485de55d2fc55fde166238077ef433
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-12-19 18:47:15 -0300

    Added react-aria button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,29 @@
+import { useButton } from '@react-aria/button'
+import { useRef } from 'react'
+
+import { FiArrowRight } from 'react-icons/fi'
 import { Container } from './styles'
 
 export function Header() {
+
+    const buttonRef = useRef()
+    const { buttonProps } = useButton({}, buttonRef)
+
+    function handleSendToGithub() {
+        window.open('https://github.com/gustavo-zsilva/singular')
+    }
+
     return (
         <Container>
             <strong>singular</strong>
+            <button
+                {...buttonProps}
+                ref={buttonRef}
+                onClick={handleSendToGithub}
+            >
+                Apoie esta ideia
+                <FiArrowRight size={22} />
+            </button>
         </Container>
     )
 }
