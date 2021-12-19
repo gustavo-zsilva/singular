28ac694c73ab714967f9894b475f318bc9e23fbb
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-12-19 18:47:02 -0300

    Added Button

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,8 +5,32 @@ export const Container = styled('header', {
     display: 'flex',
     width: '100%',
     padding: '1rem 0',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     
     'strong': {
         fontSize: '1.2rem',
+    },
+
+    'button': {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '.6rem',
+
+        border: 0,
+        borderRadius: '.2rem',
+        padding: '.6rem .8rem',
+
+        background: '$primaryColor',
+        color: 'inherit',
+        fontSize: '1.1rem',
+        cursor: 'pointer',
+        boxShadow: '0 0 3px 5px rgba(0, 0, 0, 0.03)',
+        transition: '.2s',
+
+        '&:hover': {
+            background: '$secondaryColor',
+            color: '#000',
+        }
     }
 })
